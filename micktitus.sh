06952c5c9f380de3ccdcba3b27d114b330ba217d
Mick Romanchenko <mickcole_yt@outlook.com>
2022-01-04 07:07:41 -0700

    Spelln't

--- micktitus.sh
+++ micktitus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/NickTitus/1-setup.sh
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/MickTItus/2-user.sh
+    arch-chroot /mnt /root/MickTitus/1-setup.sh
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/MickTitus/2-user.sh
     arch-chroot /mnt /root/MickTitus/3-post-setup.sh
