42be0659294ef7fad9e191cfd6980cf600490ba3
Mick Romanchenko <mickcole_yt@outlook.com>
2022-01-04 11:29:36 -0700

    More Fixes

--- micktitus.sh
+++ micktitus.sh
@@ -3,6 +3,6 @@
     bash 0-preinstall.sh
     arch-chroot /mnt /root/MickTitus/1-setup.sh
     source /mnt/root/MickTitus/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/MickTitus/2-user.sh
     arch-chroot /mnt /root/MickTitus/3-post-setup.sh
-e
+
