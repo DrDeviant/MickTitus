2fe08d8eb1f80e8abdb3eff740e039cc7486a7c6
Mick Romanchenko <mickcole_yt@outlook.com>
2022-01-19 22:50:49 +0000

    Wallpapers

---
+++ misc/configs/awesome/configuration/utils/set_wallpaper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+screen_size=$(xrandr | grep ' connected' | awk '{print $3}' | cut -f1 -d"+")
+
+IFS=$'\n'
+readarray -t <<<"$screen_size"
+feh_command="feh --bg-fill"
+
+for i in "${MAPFILE[@]}"
+do
+    feh_command="$feh_command ~/MickTitus/misc/wall.jpg"
+done
+
+eval "$feh_command"
