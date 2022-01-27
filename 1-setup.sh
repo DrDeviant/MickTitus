e7cbeeed300b42682107997749927690c1a2e89f
Mick Romanchenko <mickcole_yt@outlook.com>
2022-01-27 03:42:07 +0000

    Fix

--- 1-setup.sh
+++ 1-setup.sh
@@ -113,40 +113,12 @@ elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
 elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
     pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
 fi
-#SETUP IS WRONG THIS IS RUN
-	# Loop through user input until the user gives a valid username
-	while true
-	do 
-		read -p "Please enter username:" username
-		# username regex per response here https://unix.stackexchange.com/questions/157426/what-is-the-regex-to-validate-linux-users
-		# lowercase the username to test regex
-		if [[ "${username,,}" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
-		then 
-			break
-		fi 
-		echo "Incorrect username."
-	done 
+read -p "Please enter username:" username
 # convert name to lowercase before saving to setup.conf
-echo "username=${username,,}" >> ${HOME}/MickTitus/setup.conf
-
-    #Set Password
-    passwd $USERNAME
-    # Loop through user input until the user gives a valid hostname, but allow the user to force save 
-	while true
-	do 
-		read -p "Please name your machine:" nameofmachine
-		# hostname regex (!!couldn't find spec for computer name!!)
-		if [[ "${nameofmachine,,}" =~ ^[a-z][a-z0-9_.-]{0,62}[a-z0-9]$ ]
-		then 
-			break
-		fi 
-		# if validation fails allow the user to force saving of the hostname
-		read -p "Hostname doesn't seem correct. Do you still want to save it? (y/n)" force 
-		if [[ "${force,,}" = "y" ]]
-		then 
-			break 
-		fi 
-	done 
+echo "username=${username}" >> ${HOME}/MickTitus/setup.conf
+#Set Password
+passwd $USERNAME
+read -p "Please name your machine:" nameofmachine
 echo -ne "
 ----------------------------------------------------------------------
                     Adding User
